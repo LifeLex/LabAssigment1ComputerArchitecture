1a84c155e147245a2c812cb33d9f9a5e0e17456d
Alejandro <jandrito97@gmail.com>
2019-02-22 03:02:43 +0100

    Main
    Creada la clase main, falta creacion de objetos de tipo estudiante y profesor

--- Lab1ComputerArchitecture/main.cpp
+++ Lab1ComputerArchitecture/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
+#include <persona.h>
+#include <estudiante.h>
+#include <profesor.h>
+
+using namespace std;
+
+
 int main()
 {
 
-
+    Calificando();
     return 0;
+}
+
+void Calificando()
+{
+    //Creacion Profesor
+    Profesor *profesor= new Profesor();
+    profesor->setNombre("Martin Rekkles Larson")
+    profesor->setDNI("12345678A")
+
+    //Creacion estudiantes
+
+
 }
